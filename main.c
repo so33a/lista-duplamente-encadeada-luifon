d5bf4b1a5999578541f86ac59f7327637f46e576
Luiz Fernando Consalter Fonçatti <luifon@alunos.utfpr.edu.br>
2016-08-29 00:13:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "listaDupla.h"
+#include "ListaDupla.h"
+#include "ListaDupla.c"
 
 
-int main () {
+int main () { // professor, eu deletei algumas coisas que eram desnecessárias no código (como impressão inversa, retirada de elementos, etc
   ListaDupla l = inicializa();
   link aux, aux2;
   insereDepois(l, l->head, aux= novoNo(12, NULL, NULL));
-  aux2 = novoNo(21, NULL, NULL);
+  aux2 = novoNo(42, NULL, NULL);
   insereDepois(l,aux,aux2);
-  aux = novoNo(42, NULL, NULL); 
+  aux = novoNo(21, NULL, NULL);
   insereDepois(l, aux2, aux);
   imprime(l);
   imprimeReverso(l);
-  printf ("\nremovendo %d \n", aux2->item);
-  if((aux2 = buscar(l, 21) ) != NULL) 
-    removeNo(aux2);
+
+  link menor = buscaMenor(l);
+
+  printf("\nmenor elemento = %d\n\n", menor->item);
+
+  ordenar(l); // professor, o ordenar não vai funcionar com o valor 0 dentro de um nó da lista, por conta da inserção que utiliza o 0 como
   imprime(l);
-  destroiLista(l);
 
   return 0;
 }
